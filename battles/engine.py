1931ae29b1ccc5aec798a6dcc7d4ee9b608acb0f
Jonathan <jswrk2028@gmail.com>
2026-09-04 11:30:35 -0500

    Added remaining damage formula helpers & calculate_damage()

--- battles/engine.py
+++ battles/engine.py
@@ -1,8 +1,11 @@
 import random
-from .models import PassiveSkill, SupportEffect, BattleCreatureState
+from .models import PassiveSkill, SupportEffect, BattleCreatureState, Creature
 
 # constants
 PARTING_BLOW_LP_THRESHOLD_PERCENT = 10
+ELEMENT_CYCLE = [Creature.Element.FIRE, Creature.Element.EARTH,
+                 Creature.Element.AIR, Creature.Element.WATER]
+CRITICAL_HIT_MULTIPLIER = 1.5
 
 
 # returns true iff all 3 vivos share at least 1 TeamSkillGroup
@@ -18,6 +21,34 @@ def legal_team_skill_moves(az_creature, sz1_creature, sz2_creature):
     if not team_skill_eligible(az_creature, sz1_creature, sz2_creature):
         return az_creature.moveset.none()
     return az_creature.moveset.filter(is_team_skill=True)
+
+
+# damage calculaiton
+def calculate_damage(attacker_state, defender_state, move):
+    if move.damage is None:
+        return 0
+
+    effective_attack = _apply_parting_blow(
+        attacker_state, attacker_state.creature.attack, "attack_percent")
+    effective_defense = _apply_parting_blow(
+        defender_state, defender_state.creature.defense, "defense_percent")
+
+    attack_support = _support_multiplier(attacker_state, "attack_magnitude")
+    defense_support = _support_multiplier(defender_state, "defense_magnitude")
+
+    base = ((effective_attack + move.damage) * attack_support) - \
+        (effective_defense * defense_support)
+
+    sz_zones = (BattleCreatureState.Zone.SZ1, BattleCreatureState.Zone.SZ2)
+    range_multiplier = attacker_state.creature.sz_damage_multiplier if attacker_state.zone in sz_zones else 1.0
+
+    random_multiplier = _random_multiplier()
+    element_multiplier = _element_multiplier(
+        attacker_state.creature.element, defender_state.creature.element)
+    crit_multiplier = _crit_multiplier(attacker_state.creature.crit_rate)
+
+    damage = base * random_multiplier * element_multiplier * range_multiplier * crit_multiplier
+    return max(0, round(damage))
 
 
 # parting blow validation helper
@@ -78,6 +109,32 @@ def _support_magnitude(creature_state, magnitude_field):
     return total
 
 
-# support effect calculation helper
+# support effects calculation helper
 def _support_multiplier(creature_state, magnitude_field):
     return 1 + (_support_magnitude(creature_state, magnitude_field) / 100)
+
+
+# element calculations helper
+def _element_multiplier(attacker_element, defender_element):
+    if attacker_element not in ELEMENT_CYCLE or defender_element not in ELEMENT_CYCLE:
+        return 1.0
+
+    index = ELEMENT_CYCLE.index(attacker_element)
+    beats = ELEMENT_CYCLE[(index + 1) % 4]
+    loses_to = ELEMENT_CYCLE[(index - 1) % 4]
+
+    if defender_element == beats:
+        return 1.5
+    if defender_element == loses_to:
+        return 0.75
+    return 1.0
+
+
+# random multiplier helper
+def _random_multiplier():
+    return random.triangular(0.95, 1.05, 1.00)
+
+
+# crit multiplier helper
+def _crit_multiplier(crit_rate):
+    return CRITICAL_HIT_MULTIPLIER if random.randint(1, 100) <= crit_rate else 1.0
